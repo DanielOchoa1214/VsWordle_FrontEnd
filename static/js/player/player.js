74a1527767a024f9c65e752b459dd14630f6e9b3
DanielOchoa <danochoa1412@gmail.com>
2023-03-23 16:11:27 -0500

    Checkeo de letra desde el front

--- static/js/player/player.js
+++ static/js/player/player.js
@@ -4,49 +4,67 @@ let player = (function (api) {
     let _wordInput = $("#word-input");
     let _round = 3;
     let _nickname = "";
-    let _previousMistake = false;
+    let _mistakes = [];
+    let _currentWord = "";
 
     // Funciones privadas
     let _renderWord = (round) => {
-        let currentWord = api.getWord(round);
+        _currentWord = api.getWord(round);
         let markup = "";
         let letterPos = 0;
-        [...currentWord].forEach(letter => {
-            markup = markup.concat(`<span data-letter-pos="${letterPos}">${letter}</span>\n`)
+        [..._currentWord].forEach(letter => {
+            markup = markup.concat(`<span data-letter-pos="${letterPos}">${letter}</span>\n`);
             letterPos++;
         });
         $("#word").html(markup);
     };
 
-    let _checkLetter = () => {
-        console.log( _wordInput.val());
+    let _correctLetter = (letterPos) => {
+        $(`#word span[data-letter-pos='${letterPos}']`).removeClass("incorrect-letter");
+        $(`#word span[data-letter-pos='${letterPos}']`).addClass("correct-letter");
+        _mistakes = _mistakes.filter(x => x !== letterPos);
+    };
+
+    let _incorrectLetter = (letterPos) => {
+        $(`#word span[data-letter-pos='${letterPos}']`).removeClass("correct-letter");
+        $(`#word span[data-letter-pos='${letterPos}']`).addClass("incorrect-letter");
+        _mistakes.push(letterPos);
+    };
+
+    let _checkLetter = (letterPos, letter) => {
+        return [..._currentWord][letterPos] === letter && _mistakes.length === 0;
+    };
+
+    let _renderLetter = () => {
         let letterPos = _wordInput.val().length - 1;
         let letter = _wordInput.val().slice(-1);
-        if(api.checkLetter(letterPos, _round, letter, _nickname)){
-            $(`#word span[data-letter-pos='${letterPos}']`).removeClass("incorrect-letter");
-            $(`#word span[data-letter-pos='${letterPos}']`).addClass("correct-letter");
-            _previousMistake = false;
+        if(_checkLetter(letterPos, letter)){
+            _correctLetter(letterPos);
         } else  {
-            $(`#word span[data-letter-pos='${letterPos}']`).removeClass("correct-letter");
-            $(`#word span[data-letter-pos='${letterPos}']`).addClass("incorrect-letter");
-            _previousMistake = true;
+            _incorrectLetter(letterPos);
         }
-        
+    };
+
+    let _renderDelete = () => {
+        let letterPos = _wordInput.val().length - 1;
+        $(`#word span[data-letter-pos='${letterPos}']`).removeClass("correct-letter");
+        $(`#word span[data-letter-pos='${letterPos}']`).removeClass("incorrect-letter");
+        _mistakes = _mistakes.filter(x => x !== letterPos);
+        _wordInput.val(_wordInput.val().slice(0, -1));
     };
 
     let _deleteLetter = (event) => {
-        let letterPos = _wordInput.val().length - 1;
         var key = event.keyCode || event.charCode;
         if( key == 8 || key == 46 ){
-            $(`#word span[data-letter-pos='${letterPos}']`).removeClass("correct-letter");
-            $(`#word span[data-letter-pos='${letterPos}']`).removeClass("incorrect-letter");
+            event.preventDefault();
+            _renderDelete();
         }
-    }
+    };
 
     // Funciones publicas
     _publicFunctions.init = function () {  
         _renderWord(3);
-        _wordInput.on("input", _checkLetter);
+        _wordInput.on("input", _renderLetter);
         _wordInput.on("keydown", _deleteLetter);
     };
 
