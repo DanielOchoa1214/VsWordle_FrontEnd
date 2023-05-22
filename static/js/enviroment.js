74f5eb069119bfddcadc75e502d033a197861e2f
Wilmer Rodríguez <wilmer.rodriguez-r@mail.escuelaing.edu.co>
2023-05-22 13:12:50 -0500

    Cambio de ips

--- static/js/enviroment.js
+++ static/js/enviroment.js
@@ -1,9 +1,11 @@
 let enviroment = {
-    // local: "http://localhost:8080",
+    //local: "http://localhost:8080",
+    //events: "http://localhost:8081"
     // Apartamento
     // local: "http://192.168.20.30:8080"
     // Prod
-    local: "http://74.235.17.32:8080" 
+    local: "http://20.96.73.221:8080",
+    events: "http://20.114.181.184:8080" 
     // Universidad
     // local: "http://10.16.9.113:8080"
     // Se crea el resto cuando tengamos dominio
